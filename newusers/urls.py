0e95b0ad09247cc2e41f54ad20a9a14d38313533
Elvis Mutende <elvis@localhost.localdomain>
2019-01-09 13:38:04 -0500

    authenticate producers

--- newusers/urls.py
+++ newusers/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
 
     path('', views.index,  name='index'),
     path('producer/', views.p_login, name='producer_login'),
+    path('producerpage/', views.producer, name="producerpage"),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- newusers/views.py
+++ newusers/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib import messages
 # Create your views here.
 
 
@@ -10,4 +11,33 @@ def index(request):
 
 def p_login(request):
 
-    return render(request, 'newusers/p_login.html',{})
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username= username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in, WELCOME!')
+
+            return redirect('producerpage')
+        else:
+            messages.success(request, 'Error during login, please retry')
+            return redirect('producer_login')
+    else:
+        return render(request, 'newusers/p_login.html',{})
+
+
+def producer(request):
+
+    return render(request, 'newusers/producer_page.html',{})
+
+
+def logout_user(request):
+
+    logout(request)
+
+    messages.success(request, 'You have been logged out!')
+
+    return redirect('index')
