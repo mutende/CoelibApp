2332201bd55c451d3639a7884ff274dca6291d8a
Elvis Mutende <elvismutende@gmail.com>
2019-05-13 18:31:29 +0300

    allowed hosts

--- services/urls.py
+++ services/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 
     path('', views.services,  name='services'),
-    path('music/production', views.music_production, name='music_production'),
+    path('music/production/', views.music_production, name='music_production'),
     path('studio/session/', views.studio_session, name='studio_session'),
     path('courses/', views.short_course, name='short_course'),
     path('make_comment/', views.comment, name='comment'),
