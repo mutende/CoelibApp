6549c59a6c315d2510cae8f84d6e2a7db6821c64
Elvis Mutende <elvismutende@gmail.com>
2019-05-27 19:30:45 +0300

    admin logout redirect url

--- coelibStudio/urls.py
+++ coelibStudio/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from newusers import views
 
 urlpatterns = [
+    path('admin/logout/', views.logout_user),
     path('admin/', admin.site.urls),
     path('',include('newusers.urls')),
 ]
